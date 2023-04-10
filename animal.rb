c8e989ebddb9e67432020c8d162bb613b1e26af5
Luis Pomare <luispomare92@gmail.com>
2023-04-10 17:28:14 -0500

    Add getters and setters

--- animal.rb
+++ animal.rb
@@ -9,4 +9,24 @@ class Animal
   def speak
     "Bla bla bla"
   end
+
+  def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name = value
+  end
 end
