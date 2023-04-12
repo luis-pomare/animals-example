48a475a838240979f7b8694f4b53844b94a66ba4
Luis Pomare <luispomare92@gmail.com>
2023-04-12 18:32:21 -0500

    Create bidirectional animal owner relationship

--- animal.rb
+++ animal.rb
@@ -2,14 +2,14 @@ require "./remover.rb"
 require "./foods.rb"
 
 class Animal  
-  attr_accessor :owner
+  attr_reader :owner
 
   def initialize(type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @type = type
     @number_of_legs = number_of_legs
-    @owner= owner
+    @owner
     @liked_food = NoFood.new()
   end
   
@@ -44,5 +44,10 @@ class Animal
 
   def likes_food?(food)
     @liked_food.is_liked?(food)
+  end
+
+  def owner=(owner)
+    @owner = owner
+    owner.animals.push(self) unless owner.animals.include?(self)
   end
 end
